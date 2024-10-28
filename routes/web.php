d9b1df09c1ff17e1874c7409054ff23968d39a87
Ldpentes <luis.david.puentes@gmail.com>
2024-10-28 08:52:22 -0500

    rutas

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,20 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/crearactivo', function () {
+    return view('crearactivo');
 });
+
+
+
+Route::get('/informacionactiv', function () {
+    return view('informacionactiv');
+});
+Route::get('/inicioadmin', function () {
+    return view('inicioadmin');
+});
+Route::get('/login', function () {
+    return view('login');
+});
+
+
